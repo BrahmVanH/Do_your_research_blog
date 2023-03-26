f3ad6037b7c113883da4da6db54096d126f62fe8
Brahm V <brahmvanh@gmail.com>
2023-03-26 17:03:25 -0400

    Created view-all-posts route, added link in nav bar. Can link through homepage and load all posts in preview cards.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,4 +29,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/view-all-posts', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      limit: 5,
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const posts = blogData.map((post) => post.get({ plain: true }));
+
+    res.render('view-all-posts', {
+      posts,
+      // To convey login status of user to Auth functions
+      // To convey user information to homepage for customization
+      //logged_in: req.session.logged_in,
+      // username: req.session.username,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
